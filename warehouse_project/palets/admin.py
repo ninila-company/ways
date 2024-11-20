24878633e4630b3bea4652a0cff660aee95f81fa
ninila-company <ninila_company@bk.ru>
2024-11-20 22:47:57 +0500

    добавление возможности печать файла

--- warehouse_project/palets/admin.py
+++ warehouse_project/palets/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
-from .models import Palet
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from weasyprint import HTML
+
+from .models import Palet, Poducts_in_palet
 
 
 @admin.register(Palet)
 class PaletAdmin(admin.ModelAdmin):
-    list_display = ("number", "description", "count", "pallets_from_the_date", "pallet_pick_up_date", "receipt_mark")
+    list_display = ("number", "pallets_from_the_date", "pallet_pick_up_date", "receipt_mark", "get_products_list")
     list_filter = ("pallets_from_the_date", "pallet_pick_up_date", "receipt_mark")
+    filter_horizontal = ("description",)
+    search_fields = ("description__product_name", "number")
+    actions = ["print_selected_palets"]
+
+    def get_products_list(self, obj):
+        products = [product.product_name for product in obj.description.all()]
+        return "<br>".join(products)
+
+    get_products_list.short_description = "Продукты"
+    get_products_list.allow_tags = True
+
+    def print_selected_palets(self, request, queryset):
+        # Получаем все выбранные палеты
+        try:
+            palets = queryset
+            html_string = render_to_string('palets/print_selected_palets.html', {'palets': palets})
+            pdf_file = HTML(string=html_string).write_pdf()
+
+            response = HttpResponse(pdf_file, content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename="selected_palets.pdf"'
+            return response
+        except Exception as e:
+            self.message_user(request, f"Ошибка при генерации PDF: {str(e)}", level='error')
+            return
+
+    print_selected_palets.short_description = "Печать выбранных палет"
+
+
+@admin.register(Poducts_in_palet)
+class Poducts_in_paletAdmin(admin.ModelAdmin):
+    list_display = ("product_name",)
--- warehouse_project/palets/models.py
+++ warehouse_project/palets/models.py
@@ -1,13 +1,25 @@
 from django.db import models
+from django.utils.html import format_html
+
+
+class Poducts_in_palet(models.Model):
+    product_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.product_name
 
 
 class Palet(models.Model):
     number = models.IntegerField(verbose_name='Номер палеты')
-    description = models.CharField(max_length=255)
-    count = models.PositiveIntegerField(default=0)
+    description = models.ManyToManyField(Poducts_in_palet)
     pallets_from_the_date = models.DateField()
-    pallet_pick_up_date = models.DateField()
+    pallet_pick_up_date = models.DateField(blank=True, null=True)
     receipt_mark = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.number)
+
+    def get_products_list(self):
+        products = [product.product_name for product in self.description.all()]
+        return format_html("<br>".join(products))
+    get_products_list.short_description = "Продукты"
